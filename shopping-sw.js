18845a662a9c1d19db0ece9573f3ad1dbfda0cfc
ivancvek <ivancvej@gmail.com>
2025-11-24 10:44:00 +0100

    Add files via upload

---
+++ shopping-sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'shopping-list-v1';
+const urlsToCache = [
+  '/',
+  '/shopping-list.html',
+  '/shopping-manifest.json',
+  '/shopping-icon-192.png',
+  '/shopping-icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.log('Cache failed:', err))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+      .catch(() => {
+        return caches.match('/shopping-list.html');
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
